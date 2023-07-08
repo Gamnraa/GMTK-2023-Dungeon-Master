2db0f25a7b8e8710e24fc66056fe5c2a2b2ae0f4
Gamnraa <ltaz1466@gmail.com>
2023-07-07 23:08:21 -0400

    Nothing crazy so far.
    Uhhhh I thought I gitignored for Godot what are you doing Github

--- party.gd
+++ party.gd
@@ -1,9 +1,32 @@
 extends Node2D
 
+var alive_members
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	start()
+	
+func start():
+	alive_members = []
+	#This is fine so long the only children of this node are our chars
+	for child in get_children():
+		child.start()
+		alive_members.append(child)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_man_at_arms_dead():
+	$man_at_arms.dead = true
+	alive_members.erase($man_at_arms)
+
+
+func _on_paladin_dead():
+	$paladin.dead = true
+	alive_members.erase($paladin)
+
+func _on_cleric_dead():
+	$cleric.dead = true
+	alive_members.erase($cleric)
